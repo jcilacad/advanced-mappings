6226ab7764be0e123eaee29cc57ff78dead592c4
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 20:06:01 +0800

    Create a repository for instructor

---
+++ src/main/java/com/ilacad/AdvancedMappings/repository/InstructorRepository.java
@@ -0,0 +1,10 @@
+package com.ilacad.AdvancedMappings.repository;
+
+import com.ilacad.AdvancedMappings.entity.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+
+}
