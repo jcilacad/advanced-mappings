272e8b94f1c654fa9fbbcfb3a1908bbce483dd25
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 22:19:02 +0800

    Create an implementation for instructor service

---
+++ src/main/java/com/ilacad/AdvancedMappings/service/InstructorServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ilacad.AdvancedMappings.service;
+
+import com.ilacad.AdvancedMappings.dto.InstructorDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstructorServiceImpl implements InstructorService{
+
+    @Override
+    public void saveInstructor(InstructorDto instructorDto) {
+        
+
+    }
+}
