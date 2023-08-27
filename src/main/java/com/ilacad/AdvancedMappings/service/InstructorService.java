420efde621e4d429e4751573a17e3cec635f5696
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 22:16:34 +0800

    Create an instructor service

---
+++ src/main/java/com/ilacad/AdvancedMappings/service/InstructorService.java
@@ -0,0 +1,8 @@
+package com.ilacad.AdvancedMappings.service;
+
+import com.ilacad.AdvancedMappings.dto.InstructorDto;
+
+public interface InstructorService {
+
+    void saveInstructor(InstructorDto instructorDto);
+}
