bdae4c6ab3c9dd93f0b0101781713fc1a7034f78
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 17:49:54 +0800

    setup project

---
+++ src/main/java/com/ilacad/AdvancedMappings/AdvancedMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.ilacad.AdvancedMappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedMappingsApplication.class, args);
+	}
+
+}
