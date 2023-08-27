c4e11cd32843e8de0b5d379e31eeb4e9cd336958
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 21:52:15 +0800

    Create home page and dto for instructor

---
+++ src/main/java/com/ilacad/AdvancedMappings/dto/InstructorDto.java
@@ -0,0 +1,20 @@
+package com.ilacad.AdvancedMappings.dto;
+
+import lombok.Data;
+
+@Data
+public class InstructorDto {
+
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String youtubeChannel;
+
+    private String hobby;
+}
