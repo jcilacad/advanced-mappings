24dccf24afdde92514912a991e09a006b00aeea3
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 22:39:25 +0800

    Add binding result and model interface in post mapping of saving an instructor for validation

--- src/main/java/com/ilacad/AdvancedMappings/controller/InstructorController.java
+++ src/main/java/com/ilacad/AdvancedMappings/controller/InstructorController.java
@@ -4,6 +4,7 @@ import com.ilacad.AdvancedMappings.dto.InstructorDto;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,13 @@ public class InstructorController {
     }
 
     @PostMapping("/save-instructor")
-    public String saveInstructor(@Valid @ModelAttribute(name = "instructor") InstructorDto instructorDto) {
+    public String saveInstructor(@Valid @ModelAttribute(name = "instructor") InstructorDto instructorDto, BindingResult result, Model model) {
+        
+        // Display an error message if user provides an empty or null value/s.
+        if (result.hasErrors()) {
+            model.addAttribute("instructor", instructorDto);
+            return "home";
+        }
 
         return "redirect:/?success";
     }
