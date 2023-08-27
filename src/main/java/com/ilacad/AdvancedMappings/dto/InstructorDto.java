d15c5ab3c5792653bd2f2bb2803b116b98144373
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-27 21:56:15 +0800

    Fix the validation of instructor dto

--- src/main/java/com/ilacad/AdvancedMappings/dto/InstructorDto.java
+++ src/main/java/com/ilacad/AdvancedMappings/dto/InstructorDto.java
@@ -1,17 +1,22 @@
 package com.ilacad.AdvancedMappings.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class InstructorDto {
 
-
     private Long id;
 
+    @NotEmpty(message = "First name must not be empty")
     private String firstName;
 
+    @NotEmpty(message = "Last name must not be empty")
     private String lastName;
 
+    @Email
+    @NotEmpty(message = "Email must not be empty")
     private String email;
 
     private String youtubeChannel;
