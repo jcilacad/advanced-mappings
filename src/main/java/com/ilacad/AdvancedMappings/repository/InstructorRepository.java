11b8c155feba90b4fdf2d1da273c9707c3318cb5
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-28 19:08:50 +0800

    Create a method to check if instructor exists by email

--- src/main/java/com/ilacad/AdvancedMappings/repository/InstructorRepository.java
+++ src/main/java/com/ilacad/AdvancedMappings/repository/InstructorRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
 
+    boolean existsByEmail(String email);
 }
