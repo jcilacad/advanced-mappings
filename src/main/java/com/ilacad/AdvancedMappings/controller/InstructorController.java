e58a5b698921e7a2bc020d3382ac24be52207907
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-29 17:11:27 +0800

    Fix the route to have a default home page in /

--- src/main/java/com/ilacad/AdvancedMappings/controller/InstructorController.java
+++ src/main/java/com/ilacad/AdvancedMappings/controller/InstructorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/instructor")
+@RequestMapping({"/instructor", "/"})
 public class InstructorController {
 
     InstructorService instructorService;
