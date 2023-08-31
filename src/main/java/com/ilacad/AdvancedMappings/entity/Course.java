acc538afed6ffde44172065ffad33dcfa16c23ca
jcilacad <johnchristopherilacad27@gmail.com>
2023-08-31 15:36:15 +0800

    Create an entity for course

---
+++ src/main/java/com/ilacad/AdvancedMappings/entity/Course.java
@@ -0,0 +1,30 @@
+package com.ilacad.AdvancedMappings.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id", referencedColumnName = "id")
+    private Instructor instructor;
+
+    public Course (String title) {
+        this.title = title;
+    }
+}
