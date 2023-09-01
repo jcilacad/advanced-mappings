f10c11f8d8dd64f50296317be1a9896c31992a48
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-01 23:25:23 +0800

    Create a query to optimize the querying of instructor and courses at the same time using join fetch

--- src/main/java/com/ilacad/AdvancedMappings/repository/InstructorDetailRepository.java
+++ src/main/java/com/ilacad/AdvancedMappings/repository/InstructorDetailRepository.java
@@ -1,10 +1,14 @@
 package com.ilacad.AdvancedMappings.repository;
 
+import com.ilacad.AdvancedMappings.entity.Instructor;
 import com.ilacad.AdvancedMappings.entity.InstructorDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InstructorDetailRepository extends JpaRepository<InstructorDetail, Long> {
-
+    @Query("SELECT instructor FROM Instructor instructor JOIN FETCH instructor.courses WHERE instructor.id = :id")
+    Instructor findInstructorByIdJoinFetch(@Param("id") Long id);
 }
