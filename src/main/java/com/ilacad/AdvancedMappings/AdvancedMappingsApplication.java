5a9e661b41afbba467dd8c6c7d848249515e0a9b
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-04 21:40:46 +0800

    Remove unnecessary comments in main app

--- src/main/java/com/ilacad/AdvancedMappings/AdvancedMappingsApplication.java
+++ src/main/java/com/ilacad/AdvancedMappings/AdvancedMappingsApplication.java
@@ -1,10 +1,7 @@
 package com.ilacad.AdvancedMappings;
 
-import com.ilacad.AdvancedMappings.service.InstructorService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdvancedMappingsApplication {
