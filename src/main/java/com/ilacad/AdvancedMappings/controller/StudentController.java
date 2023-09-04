6e374ea66a0ba378ba01f4c6a66ddd92ce09e7d7
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-04 21:50:50 +0800

    Initialize the list page for student

--- src/main/java/com/ilacad/AdvancedMappings/controller/StudentController.java
+++ src/main/java/com/ilacad/AdvancedMappings/controller/StudentController.java
@@ -1,12 +1,16 @@
 package com.ilacad.AdvancedMappings.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-
+    @GetMapping("/list")
+    public String list() {
+        return "student-list";
+    }
 
 }
