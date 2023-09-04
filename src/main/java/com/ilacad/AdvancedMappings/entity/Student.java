4420d6f23c8d55fcc92499541b408c46b36c95c4
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-04 15:07:26 +0800

    Add a student entity

---
+++ src/main/java/com/ilacad/AdvancedMappings/entity/Student.java
@@ -0,0 +1,24 @@
+package com.ilacad.AdvancedMappings.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}
