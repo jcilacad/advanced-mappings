f886a90d9c63c4156061fe88611c8cf54d0e19e0
jcilacad <johnchristopherilacad27@gmail.com>
2023-09-05 09:56:39 +0800

    Add a validation of the course name is not exisiting

--- src/main/java/com/ilacad/AdvancedMappings/controller/StudentController.java
+++ src/main/java/com/ilacad/AdvancedMappings/controller/StudentController.java
@@ -56,6 +56,9 @@ public class StudentController {
         }
 
 
+        if (courses.isEmpty()) {
+            return "redirect:/student/list?unknownTitle";
+        }
         model.addAttribute("courses", courses);
 
         return "student-list";
